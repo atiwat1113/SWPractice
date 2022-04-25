a40ed4262c4faac961b559655019b8405c1b8f5f
atiwat1113 <atiwat.deepo@gmail.com>
2022-04-25 23:47:12 +0700

    fix create hospital status code

--- controllers/hospitals.js
+++ controllers/hospitals.js
@@ -105,7 +105,7 @@ exports.getHospital = async (req, res, next) => {
 
 exports.createHospital = async (req, res, next) => {
   const hospital = await Hospital.create(req.body);
-  res.status(200).json({
+  res.status(201).json({
     success: true,
     data: hospital,
   });
